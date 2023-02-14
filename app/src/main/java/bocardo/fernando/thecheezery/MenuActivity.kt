3f4e850faa076f20e97896dac940b172bad89b53
Fernando <fernando.bocardo228476@potros.itson.edu.mx>
2023-02-14 00:15:46 -0700

    The Cheezery

--- app/src/main/java/bocardo/fernando/thecheezery/MenuActivity.kt
+++ app/src/main/java/bocardo/fernando/thecheezery/MenuActivity.kt
@@ -11,10 +11,30 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweet: Button = findViewById(R.id.button_sweets) as Button
+        var btnSaltie: Button = findViewById(R.id.button_salties) as Button
 
         btnCold.setOnClickListener {
             var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "coldDrinks")
             startActivity(intent)
         }
+        btnHot.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "hotDrinks")
+            startActivity(intent)
+        }
+        btnSweet.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "sweets")
+            startActivity(intent)
+        }
+        btnSaltie.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("tipo", "salties")
+            startActivity(intent)
+        }
+
     }
 }
